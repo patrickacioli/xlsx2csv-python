83004ba64cca6aba096dd9d1a9e6997aa7e5d469
Patrick Acioli <patrick@acioli.dev>
2021-03-10 09:54:40 -0300

    initial commit

---
+++ convert.py
@@ -0,0 +1,18 @@
+import csv
+import click
+import openpyxl
+from os import listdir
+from os.path import isfile, join
+
+def read_files():
+    return [f for f in listdir("input") if isfile(join("input", f))]
+
+for file in read_files():
+    wb = openpyxl.load_workbook(f"input/{file}", read_only=True)
+    for worksheet in wb.worksheets:
+        ws = wb[worksheet.title]
+        with open(f'output/{file.replace(".xlsx", ".csv")}', "w") as out:
+            writer = csv.writer(out)
+            for row in ws:
+                values = (cell.value for cell in row)
+                writer.writerow(list(values))
